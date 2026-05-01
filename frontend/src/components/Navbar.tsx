2cf40de6e07284aef21f55f34ffc28d6752c3020
bn-khalil <benkhaliltouda3201@gmail.com>
2026-05-01 12:10:49 +0100

    prepare: cleaning for release/v1.1

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { PATHS } from "../routes/paths"
-import { useAuth } from "../context/AuthContext";
 
 interface NavItem {
     itemName: string,
--- frontend/src/services/Api.ts
+++ frontend/src/services/Api.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-console.log(BASE_URL)
-
 export const apiClient = axios.create({
     baseURL: BASE_URL,
     headers: {
