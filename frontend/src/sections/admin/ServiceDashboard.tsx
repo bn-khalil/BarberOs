5b97db0c355f19bfeaaf8e92f2975ec4df09181e
bn-khalil <benkhaliltouda3201@gmail.com>
2026-05-07 00:08:59 +0100

    feat: list all services dynamicly and show them in section with loading items

--- frontend/src/sections/admin/ServiceDashboard.tsx
+++ frontend/src/sections/admin/ServiceDashboard.tsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { FaCirclePlus } from "react-icons/fa6";
-import {services} from '../ServiceSection'
+// import {services} from '../ServiceSection'
 import { FaEdit } from "react-icons/fa";
+import { getAllServices } from '../../services/ServiceService';
 
+export interface ServiceData{
+    title: string,
+    description: string,
+    price: number,
+    duration: number,
+    image: string
+}
+
+const renderLoadingItems = (number: number) => {
+    return Array.from({ length: number }).map((_, index) => (
+        <div key={index} className="bg-main-second border border-white/5 overflow-hidden shadow-xl">
+            <div className="relative w-full h-40 bg-white/5 animate-pulse">
+                <div className="w-full h-full bg-linear-to-r from-transparent via-white/5 to-transparent animate-shimmer"></div>
+            </div>
+
+            <div className="p-2 flex gap-4 items-center justify-between">
+                <div className="h-4 w-24 bg-white/10 rounded animate-pulse"></div>
+                <div className="h-4 w-4 bg-gold/20 rounded-sm animate-pulse"></div>
+            </div>
+        </div>
+    ));
+};
 
 function ServiceDashboard() {
+
+    const [services, setServices] = useState<ServiceData[]>([]);
+    const [loading, setLoading] = useState(18);
+
+    useEffect(()=>{
+        const fetchData = async()=>{
+            const response = await getAllServices();
+            if (response) {
+                setLoading(0);
+                setServices(response.data);
+            }
+        }
+        fetchData();
+    },[])
   return (
     <div className=''>
         <div className="flex items-center justify-between">
             <h1 className='font-sans text-gold text-xl'>Services</h1>
-            <div className="flex items-center font-bold bg-gold text-main-second p-2 cursor-pointer">
+            <div className="flex items-center font-bold bg-gold text-main-second p-2 px-4 cursor-pointer">
                 <p>
                     Add
                 </p>
@@ -19,25 +56,30 @@ function ServiceDashboard() {
             </div>
         </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4 py-4">
-                {services.map((service, index) => (
-                    <div key={index} className="bg-main-second border border-white/5 rounded-sm overflow-hidden group hover:border-gold/50 transition-all duration-300 shadow-xl cursor-pointer">
-                        <div className="relative w-full h-40 overflow-hidden">
-                            <img 
-                                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" 
-                                src={service.image} 
-                                alt={service.title} 
-                            />
-                            <div className="absolute inset-0 bg-black/20 group-hover:bg-transparent transition-colors"></div>
-                        </div>
-
-                        <div className="p-2 flex gap-4 items-center justify-between">
-                            <h3 className="text-txt-col/70 font-sans text-xs tracking-wide uppercase">
-                                {service.title}
-                            </h3>
-                            <FaEdit className='text-gold'/>
-                        </div>
-                    </div>
-                ))}
+                {
+                    !loading ? (
+                        services?.map((service, index) => (
+                            <div key={index} className="bg-main-second border border-white/5 rounded-sm overflow-hidden group hover:border-gold/50 transition-all duration-300 shadow-xl cursor-pointer">
+                                <div className="relative w-full h-40 overflow-hidden">
+                                    <img 
+                                        className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" 
+                                        src={service.image} 
+                                        alt={service.title} 
+                                    />
+                                    <div className="absolute inset-0 bg-black/20 group-hover:bg-transparent transition-colors"></div>
+                                </div>
+    
+                                <div className="p-2 flex gap-4 items-center justify-between">
+                                    <h3 className="text-txt-col/70 font-sans text-xs tracking-wide uppercase">
+                                        {service.title}
+                                    </h3>
+                                    <FaEdit className='text-gold'/>
+                                </div>
+                            </div>
+                        ))
+                    ):
+                    renderLoadingItems(loading)
+                }
             </div>
     </div>
   )
